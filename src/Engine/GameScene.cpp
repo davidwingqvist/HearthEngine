b4cd427975c21ad6aa24f35fd07850b7060b7396
davidwingqvist <dw123gorde@gmail.com>
2024-04-10 20:53:43 +0900

    Awake function for script in Game Scene.

--- src/Engine/GameScene.cpp
+++ src/Engine/GameScene.cpp
@@ -1,6 +1,7 @@
 #include "Header.h"
 #include "GameScene.h"
 #include "LuaState.h"
+#include "LuaGameState.h"
 
 void GameScene::Update()
 {
@@ -9,7 +10,15 @@ void GameScene::Update()
 
 void GameScene::Awake()
 {
+	m_registry.View<Script>().ForEach([&](const recs::Entity& entity, Script& script) {
 
+		LUA_GAME.SetCurrentEntity(entity);
+		for (int i = 0; i < 5; i++)
+		{
+			LUA_GAME.AwakeObjectFromScript(script.script_id[i], entity);
+		}
+
+		});
 }
 
 void GameScene::Draw()
